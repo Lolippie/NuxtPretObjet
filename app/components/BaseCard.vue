df96a8fa0fb98932f6d09b3daf2283a23043cabb
remcor <r.cornet@myskolae.fr>
2026-03-25 09:49:36 +0100

    feat: card css
    # Conflicts:
    #	app/assets/css/main.css

--- app/components/BaseCard.vue
+++ app/components/BaseCard.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div
-      class="box-border w-80 rounded-[14px] bg-gray-900 p-6 text-gray-200 shadow-[0_20px_40px_rgba(0,0,0,0.5)] font-sans [&.selected]:shadow-[0_0_0_2px_#86efac,0_20px_40px_rgba(0,0,0,0.5)]"
+      class="base-card"
   >
     <header class="mb-4 text-center">
       <slot name="header" />
